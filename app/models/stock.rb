30bf27e03a9460e9db8bd65e068dd7552007cec0
yosif.genchev@softwareag.com <Yosif.Genchev@softwareag.com>
2019-01-12 01:47:04 +0200

    Refactoring. DB query changes.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,23 +4,40 @@ class Stock < ApplicationRecord
 
 	validates_uniqueness_of :symbol
 
-  # def last_two_stats
-  #   stats.order(created_at: :desc).limit(2)
-  # end
+  @dividend_change_percent
+  @im_index
 
-  # def dividend_change
-  # 	last_two_stats[0].dividend_yield - last_two_stats[1].dividend_yield
-  # end
+  def last_two_stats
+    stats.order(created_at: :desc).limit(2)
+  end
 
-  # def dividend_change_percentage
-  # 	((dividend_change() * 100) / last_two_stats[0].dividend_yield).round(2)
-  # end
+  def dividend_change
+  	last_two_stats[0].dividend_yield - last_two_stats[1].dividend_yield
+  end
+
+  def dividend_change_percentage
+
+    if @dividend_change_percent.nil?
+  	   @dividend_change_percent = ((dividend_change() * 100) / last_two_stats[0].dividend_yield).round(2)
+    end
+
+    @dividend_change_percent
+  end
+
+  def last_dividend_yield
+    if @last_dividend_yield.nil?
+      @last_dividend_yield = stats.last.dividend_yield
+    end
+    @last_dividend_yield
+  end
+
 
   def im_index
     if (dgr.present?)
-      ((dgr.dgr_1 + dgr.dgr_3 + dgr.dgr_5 + dgr.dgr_10 + dgr.dgr_10)/5 + dgr.mr_inc + stats.order(created_at: :desc).limit(1)[0].dividend_yield**2).round(1)
+      @im_index = ((dgr.dgr_1 + dgr.dgr_3 + dgr.dgr_5 + dgr.dgr_10 + dgr.dgr_10)/5 + dgr.mr_inc + last_dividend_yield**2).round(1)
     else
-      0
+      @im_index = 0
     end
+    @im_index
   end
 end
