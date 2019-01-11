c2aa0ec86615fc51463833f68f0dbf76dadf6182
yosif.genchev@softwareag.com <Yosif.Genchev@softwareag.com>
2019-01-11 21:12:31 +0200

    Change config.assets.compile = true

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :dgrs
   resources :stats
   resources :stocks
+  root :to => 'stocks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
