f67f85a21c63567ff557c7d2cb3494c5ddd496a9
Radoslav <r.genchew@gmail.com>
2019-01-12 20:08:55 +0100

    do some refactoring in Stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,33 +11,25 @@ class Stock < ApplicationRecord
     stats.order(created_at: :desc).limit(2)
   end
 
-  def dividend_change
-  	last_two_stats[0].dividend_yield - last_two_stats[1].dividend_yield
-  end
-
   def dividend_change_percentage
-
-    if @dividend_change_percent.nil?
-  	   @dividend_change_percent = ((dividend_change() * 100) / last_two_stats[0].dividend_yield).round(2)
-    end
-
-    @dividend_change_percent
+    ((dividend_change * 100) / last_two_stats[0].dividend_yield).round(2) if @dividend_change_percent.nil?
   end
 
   def last_dividend_yield
-    if @last_dividend_yield.nil?
-      @last_dividend_yield = stats.last.dividend_yield
-    end
-    @last_dividend_yield
+    stats.last.dividend_yield if @last_dividend_yield.nil?
   end
-
 
   def im_index
-    if (dgr.present?)
-      @im_index = ((dgr.dgr_1 + dgr.dgr_3 + dgr.dgr_5 + dgr.dgr_10 + dgr.dgr_10)/5 + dgr.mr_inc + last_dividend_yield**2).round(1)
+    if dgr.present?
+      ((dgr.dgr_1 + dgr.dgr_3 + dgr.dgr_5 + dgr.dgr_10 + dgr.dgr_10)/5 + dgr.mr_inc + last_dividend_yield**2).round(1)
     else
-      @im_index = 0
+      0
     end
-    @im_index
   end
+
+  private
+
+    def dividend_change
+      last_two_stats[0].dividend_yield - last_two_stats[1].dividend_yield
+    end
 end
